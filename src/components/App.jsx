dfb886964729cc67c4d7fade3a336837fb844164
Babytskyi-Kolya <112752847+Babytskyi-Kolya@users.noreply.github.com>
2023-09-06 10:53:47 +0300

    first-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,42 @@ import { Filter } from './filter/filter';
 import { ListContacts } from './contacts/contacts';
 import {Div, H2} from './App.Styled';
 
+// const localStorageKey = 'contacts';
+
+const initialValues = [    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+{id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+{id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+{id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},];
 
 class App extends React.Component{
   state = {
-    contacts: [],
-    name: '',
-    number: '', 
-    filter: ""
+    contacts: initialValues,
+    filter: "",
+  }
+
+
+  componentDidMount() {
+    
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({contacts : parsedContacts});
+    }
+    
+
+  }
+
+
+  componentDidUpdate (prevProps, prevState) {
+      const prevContacts = prevState.contacts;
+      const nevContacts = this.state.contacts;
+
+      if (prevContacts !== nevContacts) {
+        localStorage.setItem('contacts', JSON.stringify(nevContacts))
+      }
+
+   
   }
 
 
